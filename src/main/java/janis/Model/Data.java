c036e8daebf8f7de44cdc4645846cc44580b3a06
JanisBe <JanisBe@users.noreply.github.com>
2019-06-23 22:27:12 +0200

    init commit

---
+++ src/main/java/janis/Model/Data.java
@@ -0,0 +1,13 @@
+package janis.Model;
+
+
+@lombok.Data
+public class Data {
+
+    private int rok;
+    private int miesiac;
+    private int dzien;
+    private int godzina;
+    private int minuta;
+    private int sekunda;
+}
